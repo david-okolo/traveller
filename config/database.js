795f7f8cd9ce5af3ee39c612ac5489b933e36586
David Okolo <david.okolo@icloud.com>
2019-07-09 09:02:58 +0100

    changed database uri

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/traveller',
+    database: 'mongodb+srv://david:neVerland94%40@cluster0-9mj5w.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'mysecret'
 }
