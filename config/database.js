3587f230a15ace665f53c2cc484af40b117e831e
David Okolo <david.okolo@icloud.com>
2019-07-09 09:59:31 +0100

    Fixed: uri

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb+srv://david:neVerland94%40@cluster0-9mj5w.mongodb.net/test?retryWrites=true&w=majority',
+    database: 'mongodb+srv://david:neVerland94%40@cluster0-9mj5w.mongodb.net/traveller?retryWrites=true&w=majority',
     secret: 'mysecret'
 }
