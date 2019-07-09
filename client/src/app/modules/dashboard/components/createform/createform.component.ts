7f85657438a1f257078e1ab5b9789eafffc9f25c
David Okolo <david.okolo@icloud.com>
2019-07-09 17:04:19 +0100

    Fixed: form component

--- client/src/app/modules/dashboard/components/createform/createform.component.ts
+++ client/src/app/modules/dashboard/components/createform/createform.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CountryService } from '../../services/country.service';
+import anime from "animejs/lib/anime.es"
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-createform',
@@ -6,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./createform.component.sass']
 })
 export class CreateformComponent implements OnInit {
+
+  pop = ".23, 1.25, 0.46, 1";
 
   visas: string[] = [
     'Business',
@@ -26,6 +31,8 @@ export class CreateformComponent implements OnInit {
     'Connecting'
   ]
 
+  submitBtn: HTMLElement;
+
   //Form Data
   countryname: string;
   flightFee: number;
@@ -37,9 +44,13 @@ export class CreateformComponent implements OnInit {
   selectedVisaReqs: string[] = []
   selectedRoutes: string[] = []
 
-  constructor() { }
+  constructor(
+    private countryService: CountryService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.submitBtn = document.getElementById('submitBtn')
   }
 
   onSelect(_el, _item, _itemArray){
@@ -147,11 +158,32 @@ export class CreateformComponent implements OnInit {
     }
 
     if(status){
-      return true
+      this.submitPaper()
     }else{
-      return false
+      this.buttonAnimFailed()
     }
 
+  }
+
+  submitPaper(){
+    let form = {
+      countryName: this.countryname,
+      visaTypes: this.selectedVisas,
+      visaRequirements: this.selectedVisaReqs,
+      travelRoutes: this.selectedRoutes,
+      averageFlightPrice: this.flightFee,
+      address: this.embassyAddresses,
+      applicationLink: this.visaLink
+    }
+
+    this.countryService.postCountry(form)
+      .subscribe(data => {
+        if(data.success == true){
+          this.buttonAnimSuccess()
+        }else{
+          this.buttonAnimFailed()
+        }
+      })
   }
 
   checkValidity(_el, _data, _len){
@@ -172,5 +204,43 @@ export class CreateformComponent implements OnInit {
       default:
         break;
     }
+  }
+
+  buttonAnimSuccess(){
+    let animation = anime.timeline({})
+
+    .add({
+      targets: this.submitBtn,
+      width: "250px",
+      borderRadius: "0px",
+      backgroundColor: "#4BB543",
+      duration: 600,
+      easing: 'cubicBezier('+this.pop+')'
+    })
+
+    animation.finished.then(()=>{
+      this.router.navigate(['dashboard', 'countries']);
+    })
+  }
+
+  buttonAnimFailed(){
+    let animation = anime.timeline({})
+
+    .add({
+      targets: this.submitBtn,
+      width: "250px",
+      borderRadius: "0px",
+      backgroundColor: "#dc3545",
+      duration: 600,
+      easing: 'cubicBezier('+this.pop+')'
+    })
+    .add({
+      targets: this.submitBtn,
+      width: "200px",
+      borderRadius: "0px",
+      backgroundColor: "#1f1e48",
+      duration: 300,
+      easing: 'cubicBezier('+this.pop+')'
+    })
   }
 }
